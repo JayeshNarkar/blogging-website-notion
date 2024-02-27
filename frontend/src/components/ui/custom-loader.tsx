f5128af9003aa5e2935195fa8acd30669c4fd0ed
JayeshNarkar <jayesh.narkar18@gmail.com>
2024-02-28 01:19:50 +0530

    changed loader color

--- frontend/src/components/ui/custom-loader.tsx
+++ frontend/src/components/ui/custom-loader.tsx
@@ -3,7 +3,7 @@ import { BeatLoader } from "react-spinners";
 const CustomLoader = () => {
   return (
     <div className="flex justify-center items-center h-screen">
-      <BeatLoader color="#36d7b7" loading={true} size={10} />
+      <BeatLoader color="#007BFF" loading={true} size={10} />
     </div>
   );
 };
